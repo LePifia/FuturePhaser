317b3dff2e51cb25e619a447135e02e87ae972e4
LePifia <143432713+LePifia@users.noreply.github.com>
2024-07-13 19:02:12 +0100

    gitIgnoreChanges

--- FutureTraveller/Assets/Scripts/AudioManager.cs
+++ FutureTraveller/Assets/Scripts/AudioManager.cs
@@ -14,10 +14,6 @@ public class AudioManager : MonoBehaviour
     [SerializeField] AudioClip [] deadMoment;
     [SerializeField][Range(0f, 1f)] float casualExplosionVolume;
 
-    
-    [SerializeField] AudioClip [] jumps;
-    [SerializeField][Range(0f, 1f)] float explosionVolume;
-
     [SerializeField] AudioClip[] fireballExplode;
     [SerializeField][Range(0f, 1f)] float fireballExplodeVolume;
 
@@ -46,11 +42,6 @@ public class AudioManager : MonoBehaviour
     {
         PlayClip(fireballExplode[UnityEngine.Random.Range(0, fireballExplode.Length)], casualExplosionVolume);
 
-    }
-
-    public void Jump()
-    {
-        PlayClip(jumps[UnityEngine.Random.Range(0, jumps.Length)], casualExplosionVolume);
     }
 
     void PlayClip(AudioClip clip, float volume)
